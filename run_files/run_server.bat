939a14b536010617628cfbfed81910bbfc741cf1
Lior-Stein <liorstein1@gmail.com>
2025-08-20 11:42:21 +0300

    adding run files

---
+++ run_files/run_server.bat
@@ -0,0 +1,12 @@
+@echo off
+cd /d "C:\jars"
+echo Starting G2_server...
+java --module-path "C:\javafx-sdk-21.0.2\lib" --add-modules javafx.controls,javafx.fxml -jar G2_server.jar
+if %errorlevel% neq 0 (
+    echo.
+    echo Error: Failed to start the application
+    echo Error code: %errorlevel%
+    pause
+) else (
+    echo Application closed successfully
+)
